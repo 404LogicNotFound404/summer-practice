e1d7fa2809113fc8a4d81619d0ff0d4fbe513fb9
Andrey Rossinskij <eugeny.rossinskij@yandex.ru>
2025-07-09 10:35:07 +0600

    add plugin loading system with dependency support

---
+++ Libraries/ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using AttributeAndInterface;
+
+namespace ClassLibrary1
+{
+    [PluginLoadAttribute()]
+    public class Class1 : ICommand
+    {
+        public void Execute() 
+        {
+            Console.WriteLine("Class1");
+        }
+    }
+
+    [PluginLoadAttribute("Class1")]
+    public class Class2 : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Class2");
+        }
+    }
+
+    [PluginLoadAttribute("Class1", "Class2")]
+    public class Class3 : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Class3");
+        }
+    }
+}
---
+++ task10tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+namespace task10tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Task10Main_OutputVerification()
+        {
+            var output = new StringWriter();
+            Console.SetOut(output);
+            string ex = "\n";
+            string expected = $"Class1{ex}" +
+                $"Class4{ex}" +
+                $"Class2{ex}" +
+                $"Class5{ex}" +
+                $"Class3{ex}" +
+                $"Class6{ex}" +
+                $"Class7{ex}";
+
+            string solution = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()))))!;
+            var pluginsDirectory = Path.Combine(solution, "Libraries");
+
+            Program.Main(new[] { pluginsDirectory });
+            Assert.Equal(expected, output.ToString());
+        }
+    }
+}
