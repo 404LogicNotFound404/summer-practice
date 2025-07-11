578a1d1e7a43e805acbfb78ad1afd375d6f5f64e
Andrey Rossinskij <eugeny.rossinskij@yandex.ru>
2025-07-11 21:01:25 +0600

    fix: use Environment.NewLine

--- task10tests/UnitTest1.cs
+++ task10tests/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace task10tests
         {
             var output = new StringWriter();
             Console.SetOut(output);
-            string ex = "\n";
+            string ex = Environment.NewLine;
             string expected = $"Class1{ex}" +
                 $"Class4{ex}" +
                 $"Class2{ex}" +
